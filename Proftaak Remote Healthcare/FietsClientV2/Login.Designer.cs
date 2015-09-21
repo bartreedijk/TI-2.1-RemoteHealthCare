8e4b1823738eb811b79084215ddb2ba9e973dcb4
Mauro de Lyon <maurodelyon@gmail.com>
2015-09-21 22:13:37 +0200

    GUI V2

---
+++ Proftaak Remote Healthcare/FietsClientV2/Login.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FietsClientV2
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UsernameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // UsernameBox
+            // 
+            this.UsernameBox.Location = new System.Drawing.Point(76, 6);
+            this.UsernameBox.Name = "UsernameBox";
+            this.UsernameBox.Size = new System.Drawing.Size(223, 20);
+            this.UsernameBox.TabIndex = 2;
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(76, 36);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.PasswordChar = '♥';
+            this.PasswordBox.Size = new System.Drawing.Size(223, 20);
+            this.PasswordBox.TabIndex = 3;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(224, 79);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 4;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 114);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UsernameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UsernameBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Button SubmitButton;
+    }
+}
---
+++ Proftaak Remote Healthcare/FietsClientV2/PatientForm.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace FietsClientV2
+{
+    partial class PatientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archiefToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectSessionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sESSIONSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speedBox = new System.Windows.Forms.GroupBox();
+            this.speedChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.bpmBox = new System.Windows.Forms.GroupBox();
+            this.bpmChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.sessionInfoBox = new System.Windows.Forms.GroupBox();
+            this.rpmBox = new System.Windows.Forms.GroupBox();
+            this.rpmChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.chatArea = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.speedBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.speedChart)).BeginInit();
+            this.bpmBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bpmChart)).BeginInit();
+            this.rpmBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rpmChart)).BeginInit();
+            this.chatArea.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archiefToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1064, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archiefToolStripMenuItem
+            // 
+            this.archiefToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectSessionToolStripMenuItem});
+            this.archiefToolStripMenuItem.Name = "archiefToolStripMenuItem";
+            this.archiefToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.archiefToolStripMenuItem.Text = "Archief";
+            // 
+            // selectSessionToolStripMenuItem
+            // 
+            this.selectSessionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sESSIONSToolStripMenuItem});
+            this.selectSessionToolStripMenuItem.Name = "selectSessionToolStripMenuItem";
+            this.selectSessionToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.selectSessionToolStripMenuItem.Text = "Select Session";
+            // 
+            // sESSIONSToolStripMenuItem
+            // 
+            this.sESSIONSToolStripMenuItem.Name = "sESSIONSToolStripMenuItem";
+            this.sESSIONSToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.sESSIONSToolStripMenuItem.Text = "SESSIONS";
+            // 
+            // speedBox
+            // 
+            this.speedBox.Controls.Add(this.speedChart);
+            this.speedBox.Location = new System.Drawing.Point(12, 27);
+            this.speedBox.Name = "speedBox";
+            this.speedBox.Size = new System.Drawing.Size(400, 310);
+            this.speedBox.TabIndex = 2;
+            this.speedBox.TabStop = false;
+            this.speedBox.Text = "Speed:";
+            // 
+            // speedChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.speedChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.speedChart.Legends.Add(legend1);
+            this.speedChart.Location = new System.Drawing.Point(6, 19);
+            this.speedChart.Name = "speedChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.speedChart.Series.Add(series1);
+            this.speedChart.Size = new System.Drawing.Size(388, 285);
+            this.speedChart.TabIndex = 0;
+            this.speedChart.Text = "Speed chart";
+            // 
+            // bpmBox
+            // 
+            this.bpmBox.Controls.Add(this.bpmChart);
+            this.bpmBox.Location = new System.Drawing.Point(12, 343);
+            this.bpmBox.Name = "bpmBox";
+            this.bpmBox.Size = new System.Drawing.Size(400, 310);
+            this.bpmBox.TabIndex = 3;
+            this.bpmBox.TabStop = false;
+            this.bpmBox.Text = "Beats per second:";
+            // 
+            // bpmChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.bpmChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.bpmChart.Legends.Add(legend2);
+            this.bpmChart.Location = new System.Drawing.Point(6, 19);
+            this.bpmChart.Name = "bpmChart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.bpmChart.Series.Add(series2);
+            this.bpmChart.Size = new System.Drawing.Size(388, 285);
+            this.bpmChart.TabIndex = 1;
+            this.bpmChart.Text = "beats per second";
+            // 
+            // sessionInfoBox
+            // 
+            this.sessionInfoBox.Location = new System.Drawing.Point(418, 27);
+            this.sessionInfoBox.Name = "sessionInfoBox";
+            this.sessionInfoBox.Size = new System.Drawing.Size(400, 310);
+            this.sessionInfoBox.TabIndex = 4;
+            this.sessionInfoBox.TabStop = false;
+            this.sessionInfoBox.Text = "Session info:";
+            // 
+            // rpmBox
+            // 
+            this.rpmBox.Controls.Add(this.rpmChart);
+            this.rpmBox.Location = new System.Drawing.Point(418, 343);
+            this.rpmBox.Name = "rpmBox";
+            this.rpmBox.Size = new System.Drawing.Size(400, 310);
+            this.rpmBox.TabIndex = 5;
+            this.rpmBox.TabStop = false;
+            this.rpmBox.Text = "Rounds per minute:";
+            // 
+            // rpmChart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.rpmChart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.rpmChart.Legends.Add(legend3);
+            this.rpmChart.Location = new System.Drawing.Point(6, 19);
+            this.rpmChart.Name = "rpmChart";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.rpmChart.Series.Add(series3);
+            this.rpmChart.Size = new System.Drawing.Size(388, 285);
+            this.rpmChart.TabIndex = 2;
+            this.rpmChart.Text = "rounds per minute";
+            // 
+            // chatBox
+            // 
+            this.chatBox.Location = new System.Drawing.Point(0, 19);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.ReadOnly = true;
+            this.chatBox.Size = new System.Drawing.Size(228, 581);
+            this.chatBox.TabIndex = 3;
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(0, 606);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(228, 20);
+            this.messageBox.TabIndex = 6;
+            // 
+            // chatArea
+            // 
+            this.chatArea.Controls.Add(this.chatBox);
+            this.chatArea.Controls.Add(this.messageBox);
+            this.chatArea.Location = new System.Drawing.Point(824, 27);
+            this.chatArea.Name = "chatArea";
+            this.chatArea.Size = new System.Drawing.Size(228, 626);
+            this.chatArea.TabIndex = 5;
+            this.chatArea.TabStop = false;
+            this.chatArea.Text = "Chat:";
+            // 
+            // PatientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.chatArea);
+            this.Controls.Add(this.sessionInfoBox);
+            this.Controls.Add(this.rpmBox);
+            this.Controls.Add(this.bpmBox);
+            this.Controls.Add(this.speedBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PatientForm";
+            this.Text = "Patient";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.speedBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.speedChart)).EndInit();
+            this.bpmBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bpmChart)).EndInit();
+            this.rpmBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rpmChart)).EndInit();
+            this.chatArea.ResumeLayout(false);
+            this.chatArea.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archiefToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectSessionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sESSIONSToolStripMenuItem;
+        private System.Windows.Forms.GroupBox speedBox;
+        private System.Windows.Forms.GroupBox bpmBox;
+        private System.Windows.Forms.GroupBox sessionInfoBox;
+        private System.Windows.Forms.GroupBox rpmBox;
+        private System.Windows.Forms.DataVisualization.Charting.Chart speedChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart bpmChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart rpmChart;
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.GroupBox chatArea;
+    }
+}
+
