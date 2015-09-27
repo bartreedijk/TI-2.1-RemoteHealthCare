641faf9352bac545aae3a811380f52b48d97fa98
Richard Danen <titat1993@gmail.com>
2015-09-27 19:02:56 +0200

    forgot

---
+++ Proftaak Remote Healthcare/FietsClientV2/CurrentData.cs
@@ -0,0 +1,37 @@
+﻿using FietsClient.JSONObjecten;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FietsClient
+{
+    class CurrentData
+    {
+        private string userID;
+        private List<Session> testResult;
+
+
+        public CurrentData(string id)
+        {
+            this.userID = id;
+            testResult = new List<Session>();
+        }
+
+        public void setSessionList(List<Session> tests)
+        {
+            testResult = tests;
+        }
+
+        public List<Session> GetSessions()
+        {
+            return testResult;
+        }
+
+        public void setSession(Session s)
+        {
+            testResult.Add(s);
+        }
+    }
+}
---
+++ Proftaak Remote Healthcare/FietsClientV2/lib/JsonConverter.cs
@@ -0,0 +1,18 @@
+﻿using FietsClient.JSONObjecten;
+using Newtonsoft.Json;
+
+namespace FietsClient.lib
+{
+    class JsonConverter
+    {
+        public static string SerializeSession(Session s)
+        {
+            return JsonConvert.SerializeObject(s);
+        }
+
+        public static string SerializeLastMeasurement(Measurement measurement)
+        {
+            return JsonConvert.SerializeObject(measurement);
+        }
+    }
+}
